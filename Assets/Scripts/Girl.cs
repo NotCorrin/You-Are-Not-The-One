6782f1682e18e815b6746a2356e692be5747d8ed
NotCorrin <43767802+NotCorrin@users.noreply.github.com>
2020-12-01 19:01:04 +1100

    Main Architecture Complete

--- Assets/Scripts/Girl.cs
+++ Assets/Scripts/Girl.cs
@@ -6,12 +6,17 @@ public class Girl : MonoBehaviour
 {
     #region Values
     [SerializeField] string girlName;
+    [SerializeField] float atheletic;
+    [SerializeField] float nerdy;
+    [SerializeField] float empathy;
+    [SerializeField] float sensuality;
+
     public bool selected;
     #endregion
 
     void Start()
     {
-        #region 
+        #region Initial Values
         selected = false;
         #endregion
     }
